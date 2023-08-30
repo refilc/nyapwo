4b22d20e728110c7ff787090ab012d50cfd2b718
ReinerRego <regokoppany@gmail.com>
2023-08-30 15:40:06 +0200

    changed theme

---
+++ filcnaplo_uwuifier/example/filcnaplo_uwuify_example.dart
@@ -0,0 +1,6 @@
+import 'package:filcnaplo_uwuify/filcnaplo_uwuify.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ filcnaplo_uwuifier/test/filcnaplo_uwuify_test.dart
@@ -0,0 +1,16 @@
+import 'package:filcnaplo_uwuify/filcnaplo_uwuify.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
