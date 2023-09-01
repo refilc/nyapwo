5cc27721544807669449d70256a1b47797cea5d5
ReinerRego <regokoppany@gmail.com>
2023-09-01 16:29:03 +0200

    uwu subjects ^3^

--- filcnaplo_kreta_api/lib/models/subject.dart
+++ filcnaplo_kreta_api/lib/models/subject.dart
@@ -1,4 +1,7 @@
 import 'category.dart';
+import 'package:filcnaplo_uwuifier/src/filcnaplo_uwuify_base.dart';
+
+Uwuifier uwuifier = Uwuifier();
 
 class Subject {
   String id;
@@ -8,16 +11,30 @@ class Subject {
 
   bool get isRenamed => renamedTo != null;
 
+  // Constructor
   Subject({required this.id, required this.category, required this.name, this.renamedTo});
 
+  // Factory method to create Subject from JSON
   factory Subject.fromJson(Map json) {
-    final id = json["Uid"] ?? "";
+  final id = json["Uid"] ?? "";
+  final rawName = json["Nev"];
+  if (rawName is String) {
+    final name = uwuifier.uwuifySentence(rawName.trim()); // Uwuify the name
     return Subject(
       id: id,
       category: Category.fromJson(json["Kategoria"] ?? {}),
-      name: (json["Nev"] ?? "").trim(),
+      name: name,
+    );
+  } else {
+    // Handle the case where "Nev" is not a string (or null) gracefully
+    return Subject(
+      id: id,
+      category: Category.fromJson(json["Kategoria"] ?? {}),
+      name: "Unknown", // Provide a default value or handle as needed
     );
   }
+}
+
 
   @override
   bool operator ==(other) {
